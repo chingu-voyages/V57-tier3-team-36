9440b8fe2b7bb4a16d761c39df179983075e12b2
petertasca <tascapeter@gmail.com>
2025-09-11 14:42:38 -0400

    refactor: index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-import 'dotenv/config';
+import { loadEnvConfig } from '@next/env';
 import { drizzle } from 'drizzle-orm/neon-http';
-// import { eq } from 'drizzle-orm';
-// import { usersTable } from './db/schema';
+import * as schema from './db/schema'
 
-const db = drizzle(process.env.DEV_DATABASE_URL || '');
+const projectDir = process.cwd();
+loadEnvConfig(projectDir)
+
+if (!process.env.DEV_DATABASE_URL) throw new Error('DB is not set in .env.local')
+
+export const db = drizzle(process.env.DEV_DATABASE_URL, { schema });
