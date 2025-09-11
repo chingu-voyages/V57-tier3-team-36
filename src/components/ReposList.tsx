bdbddc1270d512f7e74b68b672f1137bfb6f51ad
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-09-11 19:25:23 -0400

    add example components that use auth and data

---
+++ src/components/ReposList.tsx
@@ -0,0 +1,23 @@
+import 'server-only';
+
+import { fetchGitHubRepos } from '@/lib/fetchGithubRepos';
+import { requireAuth } from '@/lib/requireAuth';
+
+export async function ReposList() {
+  const isAuthenticated = await requireAuth();
+  if (!isAuthenticated) return null;
+
+  const repos = await fetchGitHubRepos();
+
+  return (
+    <div className="flex flex-col gap-4 w-full items-center">
+      Repos: {repos.length}
+      {repos.map((repo) => (
+        <div key={repo.id} className="border p-4 rounded max-w-[600px] w-full">
+          <h3 className="font-bold">{repo.name}</h3>
+          <p className="text-gray-600">{repo.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
