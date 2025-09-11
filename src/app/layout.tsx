6b76de4045c9d6f12a5d30cca7fa9152907de61f
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-09-11 19:27:40 -0400

    add components, auth, and mocks to app pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,23 @@
-import "./globals.css";
+import 'server-only';
 
+import '@/app/globals.css';
 
-export default async function RootLayout({
+if (
+  process.env.NODE_ENV === 'development' &&
+  process.env.ENABLE_API_MOCKING === 'true'
+) {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  require('../mocks');
+}
+
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
