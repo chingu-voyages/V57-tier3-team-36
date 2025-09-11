0e2e7c7e38999e97491054c7d86ff2f626135b9e
petertasca <tascapeter@gmail.com>
2025-09-11 14:43:28 -0400

    fix: semicolons in index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './db/schema'
 
 const projectDir = process.cwd();
-loadEnvConfig(projectDir)
+loadEnvConfig(projectDir);
 
-if (!process.env.DEV_DATABASE_URL) throw new Error('DB is not set in .env.local')
+if (!process.env.DEV_DATABASE_URL) throw new Error('DB is not set in .env.local');
 
 export const db = drizzle(process.env.DEV_DATABASE_URL, { schema });
