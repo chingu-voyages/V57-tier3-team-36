e18f2c5bda1a149a0f3e020a168c10da640dbd37
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-09-10 21:48:20 -0400

    add tracing root to next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+
+  // prevent Next.js from resolving files outside the project root
+  outputFileTracingRoot: './',
 };
 
 export default nextConfig;
