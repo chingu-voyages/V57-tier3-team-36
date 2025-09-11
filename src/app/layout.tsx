f430dc21cb5258f6d68b0b0770c26dac9147841a
petertasca <tascapeter@gmail.com>
2025-09-11 10:29:14 -0400

    feat: connected to dev database

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,27 @@
 import "./globals.css";
+import { neon } from '@neondatabase/serverless'
 
-export default function RootLayout({
+
+async function getData() {
+  const sql = neon(process.env.DEV_DATABASE_URL || '');
+  const response = await sql`SELECT version()`;
+  return response[0].version;
+}
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+   const data = await getData()
+   console.log('data:', data)
   return (
     <html lang="en">
       <body>
+        <header>
+          {data}
+        </header>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
+import { neon } from '@neondatabase/serverless'
 
 
-export default function Home() {
+
+export default async function Home() {
+
+  async function create(formData: FormData) {
+    "use server";
+    if (!process.env.DEV_DATABASE_URL) throw new Error("Missing DB URL")
+    const sql = neon(process.env.DEV_DATABASE_URL);
+    await sql`CREATE TABLE IF NOT EXISTS comments (comment TEXT)`;
+    const comment = formData.get("comment");
+    await (sql.query as any)("INSERT INTO comments (comment) VALUES ($1)", [comment]);
+
+    
+  }
+ 
   return (
     <div >
       <main>
+        <form action={create}>
+          <input type="text" placeholder='write a comment' name='comment' />
+          <button type='submit'>Submit</button>
+        </form>
+        
       
       </main>
       
