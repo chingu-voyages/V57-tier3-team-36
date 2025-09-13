c906164f0775ffdaf92a64b1086c0a851afe27ed
David Eastmond <davebuck1981@gmail.com>
2025-09-13 13:14:49 -0400

    Refactor GitHub API client: replace class with function and implement pagination; add new repo actions for fetching repos, open/closed pull requests, and contributors

--- src/lib/base-github-api-client/base-github-api-client.ts
+++ src/lib/base-github-api-client/base-github-api-client.ts
@@ -1,11 +1,67 @@
-export abstract class BaseGithubApiClient {
-  private baseUrl: string = "https://api.github.com";
-
-  protected async get<T>(path: string): Promise<T> {
-    const response = await fetch(`${this.baseUrl}${path}`);
+export function BaseGithubApiClient() {
+  const baseUrl: string = "https://api.github.com";
+  const authorizationHeader = `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`;
+  async function get<T>(path: string): Promise<T> {
+    const response = await fetch(`${baseUrl}${path}`, {
+      headers: {
+        Authorization: authorizationHeader,
+      },
+    });
     if (!response.ok) {
       throw new Error(`GitHub API request failed: ${response.statusText}`);
     }
+    console.log(response.headers.get("Link"));
     return response.json();
   }
+
+  async function getWithPagination<T>(
+    path: string,
+    pageNumber: number = 1
+  ): Promise<{ data: T; nextPage?: number }> {
+    const composedPath = `${baseUrl}${path}?page=${pageNumber}`;
+    console.info(`Fetching from: ${composedPath}`);
+    const response = await fetch(composedPath, {
+      headers: {
+        Authorization: authorizationHeader,
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`GitHub API request failed: ${response.statusText}`);
+    }
+    const linkHeader = response.headers.get("Link");
+    let nextPage: number | undefined = undefined;
+
+    if (linkHeader) {
+      const links = parseLinkHeader(linkHeader);
+      if (links && links.next) {
+        const url = new URL(links.next);
+        const pageParam = url.searchParams.get("page");
+        if (pageParam) {
+          nextPage = parseInt(pageParam, 10);
+        }
+      }
+    }
+
+    const data = await response.json();
+    return { data, nextPage };
+  }
+
+  return { get, getWithPagination };
+}
+
+function parseLinkHeader(header: string): { [key: string]: string } {
+  const links: { [key: string]: string } = {};
+  const parts = header.split(",");
+
+  parts.forEach((part) => {
+    const section = part.split(";");
+    if (section.length !== 2) {
+      return;
+    }
+    const url = section[0].replace(/<(.*)>/, "$1").trim();
+    const name = section[1].replace(/rel="(.*)"/, "$1").trim();
+    links[name] = url;
+  });
+
+  return links;
 }
---
+++ src/lib/github-repo-actions/github-repo-actions.ts
@@ -0,0 +1,39 @@
+"use server";
+import { BaseGithubApiClient } from "../base-github-api-client/base-github-api-client";
+
+const githubApiClient = BaseGithubApiClient();
+
+export async function getAllReposForUsername<T>(
+  owner: string,
+  pageNumber: number = 1
+): Promise<{ data: T; nextPage?: number }> {
+  return githubApiClient.getWithPagination<T>(
+    `/users/${owner}/repos`,
+    pageNumber
+  );
+}
+export async function getOpenPullRequestsForRepo<T>(
+  owner: string,
+  repo: string
+): Promise<T> {
+  return githubApiClient.get<T>(`/repos/${owner}/${repo}/pulls?state=open`);
+}
+export async function getContributorsForRepo<T>(
+  owner: string,
+  repo: string
+): Promise<T> {
+  return githubApiClient.get<T>(`/repos/${owner}/${repo}/contributors`);
+}
+export async function getClosedPullRequestsForRepo<T>(
+  owner: string,
+  repo: string
+): Promise<T> {
+  return githubApiClient.get<T>(`/repos/${owner}/${repo}/pulls?state=closed`);
+}
+export async function getBranchesForRepo<T>(
+  owner: string,
+  repo: string
+): Promise<T[]> {
+  // return this.getPaginated<T>(`/repos/${owner}/${repo}/branches`);
+  return [];
+}
