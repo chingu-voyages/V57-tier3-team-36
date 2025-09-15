5d284eddb089a9a13eeedcbc7d5449baf952d2ec
Kelly Ripple <11916341+kripple@users.noreply.github.com>
2025-09-14 21:06:17 -0400

    Experiment/daisy UI v2 (#27)
    * Installed Daisy-UI package (visible in Package.json).
    
    * After importing Daisy-UI package, I configured page.tsx and globals.css file to be compatible with Daisy-UI components.
    
    Also, there's 2 daisy-ui components in the page.tsx file that are placeholders and can be edited as needed.
    * resolve merge collisions
    * lint
    * update package versions
    
    ---------
    
    Co-authored-by: Sattyik Kundu <sattyikkundu@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,25 @@ import { ReposList } from '@/components/ReposList';
 
 export default function HomePage() {
   return (
-    <div>
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <nav className="flex items-center justify-between px-5 py-3">
         <h1>App Name Goes Here</h1>
         <AuthButton />
       </nav>
-      <ReposList />
+
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        {/* ADDED (daisyUI): a primary button */}
+        <button className="btn btn-primary">
+          Dummy Daisy-UI component #1 (in page.tsx)
+        </button>
+
+        {/* ADDED (daisyUI): an info alert */}
+        <div className="alert alert-info">
+          <span>Dummy Daisy-UI component #2 (in page.tsx)</span>
+        </div>
+
+        <ReposList />
+      </main>
     </div>
   );
 }
