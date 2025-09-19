b59de1d60aedc74765ea783917bd83c3d37e8f31
David Eastmond <davebuck1981@gmail.com>
2025-09-19 19:52:41 -0400

    refactor: move renderBadge function inside PrReviewStateBadge for better encapsulation

--- src/components/prCard/PrReviewStateBadge.tsx
+++ src/components/prCard/PrReviewStateBadge.tsx
@@ -9,32 +9,31 @@ export const PrReviewStateBadge = ({
   avatar_url: string;
   state: string;
 }) => {
+  const renderBadge = (state: string) => {
+    if (state === "APPROVED") {
+      return (
+        <div className="badge badge-success flex">
+          <ApprovedCircleCheckIcon />
+          <p>Approved</p>
+        </div>
+      );
+    }
+    if (state === "CHANGES_REQUESTED") {
+      return (
+        <div className="badge badge-error flex">
+          <RequestedChangesIcon /> Requested Changes
+        </div>
+      );
+    }
+    if (state === "COMMENTED") {
+      return <div className="badge badge-info">Commented</div>;
+    }
+    return null;
+  };
   return (
     <div className="flex items-center gap-2">
       <UserAvatar url={avatar_url} />
       {renderBadge(state)}
     </div>
   );
-};
-
-const renderBadge = (state: string) => {
-  if (state === "APPROVED") {
-    return (
-      <div className="badge badge-success flex">
-        <ApprovedCircleCheckIcon />
-        <p>Approved</p>
-      </div>
-    );
-  }
-  if (state === "CHANGES_REQUESTED") {
-    return (
-      <div className="badge badge-error flex">
-        <RequestedChangesIcon /> Requested Changes
-      </div>
-    );
-  }
-  if (state === "COMMENTED") {
-    return <div className="badge badge-info">Commented</div>;
-  }
-  return null;
 };
