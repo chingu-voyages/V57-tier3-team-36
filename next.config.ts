aae4b48ffbcfb8b2a44514c3c78fb95671ca08e5
David Eastmond <davebuck1981@gmail.com>
2025-09-19 14:28:14 -0400

    add domain for next-image

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
@@ -6,20 +6,22 @@ const nextConfig: NextConfig = {
   env: {
     APP_VERSION:
       // eslint-disable-next-line @typescript-eslint/no-require-imports
-      process.env.npm_package_version || require('./package.json').version,
-    APP_NAME: 'MyApp',
+      process.env.npm_package_version || require("./package.json").version,
+    APP_NAME: "MyApp",
   },
-
+  images: {
+    domains: ["avatars.githubusercontent.com"],
+  },
   // Prevent Next.js from resolving files outside the project root.
   outputFileTracingRoot: __dirname,
 
   // Only load MSW in development.
   async rewrites() {
-    return process.env.NODE_ENV === 'development'
+    return process.env.NODE_ENV === "development"
       ? [
           {
-            source: '/mockServiceWorker.js',
-            destination: '/mockServiceWorker.js',
+            source: "/mockServiceWorker.js",
+            destination: "/mockServiceWorker.js",
           },
         ]
       : [];
