3a153755ae24e68e0e12cff42ec2303c6bf10d4a
Hyun Woo Kim <hynwkm@gmail.com>
2025-09-19 11:56:13 -0400

    chore: fix linter issue

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,20 +1,23 @@
-import type { components } from '@octokit/openapi-types';
+import type { components } from "@octokit/openapi-types";
 
 declare global {
-  type Result<T, E = string> =
-    | { success: true; data: T }
-    | { success: false; error: E };
+    type Result<T, E = string> =
+        | { success: true; data: T }
+        | { success: false; error: E };
 
-  // GitHub username is the `login` property
-  type GitHubUser = components['schemas']['simple-user'];
+    // GitHub username is the `login` property
+    type GitHubUser = components["schemas"]["simple-user"];
 
-  type GitHubRepo = components['schemas']['repository'];
+    type GitHubRepo = components["schemas"]["repository"];
 
-  type GitHubContributor = components['schemas']['contributor'];
+    type GitHubContributor = components["schemas"]["contributor"];
 
-  type GitHubBranch = components['schemas']['short-branch'];
+    type GitHubBranch = components["schemas"]["short-branch"];
 
-  type GitHubPullRequest = components['schemas']['pull-request'];
+    type GitHubPullRequest = components["schemas"]["pull-request"];
+
+    type GitHubSearchIssuesResponse =
+        components["schemas"]["issue-search-result-issue"];
 }
 
 export {};
