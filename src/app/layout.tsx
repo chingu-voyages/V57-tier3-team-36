49b9e75b1a36397b06f8c42da269aafd7ccbc26d
VincentBui0 <107823093+VincentBui0@users.noreply.github.com>
2025-09-22 17:14:21 -0400

    Implement footer (#29)
    * Add Footer component
    
    * Add Footer component
    
    * Images for Footer
    
    * Add Header component
    
    * Revert "Add Header component"
    
    This reverts commit 85020839c83c54ad73ce600ca1b71c64adf48f90.
    
    * Revert Header component
    
    * Update src/components/Footer.tsx
    
    Co-authored-by: David Eastmond <davebuck1981@gmail.com>
    
    * Update Footer with Provided Feedback
    
    * Update packages
    
    ---------
    
    Co-authored-by: Kelly Ripple <11916341+kripple@users.noreply.github.com>
    Co-authored-by: David Eastmond <davebuck1981@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/Footer';
 import './globals.css';
 
 if (
@@ -15,7 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+\        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
