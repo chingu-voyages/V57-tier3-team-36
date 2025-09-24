2acb67744118893c4f7a38b6551ca5ac0a200be1
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-09-24 18:11:58 -0400

    lint

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'drizzle-kit';
 
-if (!process.env.DATABASE_URL) throw new Error('Missing DATABASE_URL')
+if (!process.env.DATABASE_URL) throw new Error('Missing DATABASE_URL');
 
 export default defineConfig({
   out: './drizzle',
