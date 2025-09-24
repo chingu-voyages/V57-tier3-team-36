0d9b7a22efe5b2a2aef7b3796a36abdad5190eb3
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-09-24 18:26:30 -0400

    modify playwright config for ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
+  forbidOnly: !!process.env.CI,
+
   testDir: './src/tests',
+
+  projects: [{ name: 'chromium', use: { ...devices['Desktop Chrome'] } }],
+
+  reporter: process.env.CI ? 'dot' : 'list',
+
   use: {
     baseURL: 'http://localhost:3000',
   },
+
   webServer: {
     command: 'npm run dev',
     url: 'http://localhost:3000',
