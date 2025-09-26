b58d94b10a72cb3f9595eb6a78be524c47a3d37a
David Eastmond <davebuck1981@gmail.com>
2025-09-26 19:08:13 -0400

    building out route

--- src/app/api/users/[user_id]/route.ts
+++ src/app/api/users/[user_id]/route.ts
@@ -1,8 +1,25 @@
-import { NextRequest } from 'next/server';
+import { user } from '@/db/schema';
+import { db } from '@/index';
+import { eq } from 'drizzle-orm';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(
   request: NextRequest,
-  { params }: { params: { user_id: string } }
+  { params }: { params: Promise<{ user_id: string }> }
 ) {
-  const { user_id } = params;
+  const { user_id } = await params;
+
+  const userInDb = await db.query.user.findFirst({
+    where: eq(user.id, user_id),
+  });
+
+  // We shouldn't need this since we are authenticating the user
+  if (!userInDb) {
+    return NextResponse.json(
+      { error: `User with id ${user_id} not found`, success: false } as const,
+      { status: 404 }
+    );
+  }
+
+  return NextResponse.json({ data: userInDb, success: true }, { status: 200 });
 }
