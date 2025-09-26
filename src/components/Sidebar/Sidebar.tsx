333a8a72e5a3e6d6f408902c33a5b0439d336fb7
David Eastmond <davebuck1981@gmail.com>
2025-09-26 17:04:16 -0400

    Update the SideBar to reflect User Authentications tatus (#40)
    * Fix filename spelling
    
    * Refactor Sidebar component to integrate user authentication and display GitHub avatar
    
    * Updated auth icons
    
    * Fix button labels for consistency and update button alignment in Sidebar component
    
    * Refactor Sidebar component to inline authenticated sidebar items for improved readability

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,11 @@
-import ProfileIcon from '../icons/ProfileIcon';
+'use client';
+import { useAuth } from '@/hooks/useAuth';
+import { GitHubAvatar } from '../githubAvatar/GitHubAvatar';
+import SidebarIcon from './SidebarIcon';
 import SidebarItem from './SidebarItem';
-
 export default function Sidebar() {
+  const { user, signOut, signIn, isAuthenticated } = useAuth();
+
   return (
     <div className="drawer-side">
       <label
@@ -9,19 +13,46 @@ export default function Sidebar() {
         aria-label="close sidebar"
         className="drawer-overlay"
       ></label>
-      <ul className="menu text-base-content min-h-full w-60 p-4 ">
-        <div className="flex items-center gap-2 mb-4 justify-between text-2xl px-3 font-bold">
-          <ProfileIcon />
-          Jane Doe
-        </div>
+      <ul className="menu text-base-content min-h-full w-60 p-4 bg-base-100">
+        {isAuthenticated && (
+          <div className="flex items-center gap-2 mb-4 justify-between text-2xl px-3 font-bold">
+            <GitHubAvatar url={user.image!} />
+            {user?.name}
+          </div>
+        )}
+
         <div className="flex flex-col gap-1 font-semibold text-lg">
           <SidebarItem label="Home" />
-          <SidebarItem label="Commits" />
-          <SidebarItem label="Contributors" />
-          <SidebarItem label="Reviews" />
-          <SidebarItem label="Quality" />
+          {isAuthenticated ? (
+            <>
+              <SidebarItem label="Commits" />
+              <SidebarItem label="Contributors" />
+              <SidebarItem label="Reviews" />
+              <SidebarItem label="Quality" />
+              <AuthButton action={signOut} label="Sign Out" />
+            </>
+          ) : (
+            <AuthButton action={signIn} label="Sign In" />
+          )}
         </div>
       </ul>
     </div>
   );
 }
+
+const AuthButton = ({
+  action,
+  label,
+}: {
+  action: () => void;
+  label: 'Sign In' | 'Sign Out';
+}) => {
+  return (
+    <li>
+      <button className="w-full justify-between" onClick={action}>
+        <SidebarIcon label={label} />
+        {label}
+      </button>
+    </li>
+  );
+};
---
+++ src/components/icons/SignOutIcon.tsx
@@ -0,0 +1,14 @@
+export default function SignInIcon() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M14 16L18 12L14 8V11H4V13H14V16Z" fill="currentColor" />
+      <path d="M20 20H4V18H20V20Z" fill="currentColor" />
+    </svg>
+  );
+}
