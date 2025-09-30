099855f469875fd6a36cd027e745bb95a223f41b
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-09-30 17:51:51 -0400

    finish up responsive page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,36 @@
 export default function HomePage() {
-  const statsCardStyle =
-    'card bg-base-100 w-full sm:max-w-[50%] md:max-w-[25%] px-3 py-2 min-w-[300px]';
+  const statsCardStyle = 'card bg-base-100 w-full px-3 py-2 h-28 md:h-full';
 
   return (
-    <div className="w-full h-full grid grid-cols-12 gap-3">
-      <div data-label="MainContents" className="col-span-12 md:col-span-8 row-start-2 md:row-start-1">
-        <div data-label="SearchBar" className="input w-full mb-3 px-3 py-2">
-          Search Bar
-        </div>
-        <div data-label="PullsList" className="w-full card bg-base-100 px-3 py-2">
-          List of Pull Requests
+    <div
+      data-label="HomePage"
+      className="w-full h-full grid grid-cols-12 grid-rows-[auto_1fr] md:grid-rows-1 gap-3"
+    >
+      <div
+        data-label="MainContents"
+        className="col-span-12 md:col-span-8 xl:col-span-9 row-start-2 md:row-start-1 h-full"
+      >
+        <div
+          data-label="PullsListCard"
+          className="card flex flex-col w-full h-full bg-base-100 p-2"
+        >
+          <div
+            data-label="SearchBar"
+            className="w-full mb-2 px-3 py-2 bg-[red]"
+          >
+            Search Bar
+          </div>
+          <div
+            data-label="PullsList"
+            className="flex-grow w-full px-3 py-2 bg-[blue] overflow-y-auto"
+          >
+            List of Pull Requests
+          </div>
         </div>
       </div>
       <aside
         data-label="StatsCards"
-        className="flex flex-wrap gap-3 col-span-12 md:col-span-4 row-start-1"
+        className="grid gap-3 grid-cols-2 sm:grid-cols-4 md:grid-cols-1 col-span-12 md:col-span-4 xl:col-span-3 row-start-1"
       >
         <div data-label="StatsCard" className={statsCardStyle}>
           Open PRs
