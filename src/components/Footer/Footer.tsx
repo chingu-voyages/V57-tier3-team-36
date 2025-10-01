56aabd1085ffc719389dee6e833a58c8d9e4cd86
Kelly Ripple <11916341+kripple@users.noreply.github.com>
2025-10-01 18:29:55 -0400

    Add component organization decision to readme (#51)
    * Add component organization decision to readme
    
    * reorganize components according to team decision

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Image from 'next/image';
-import GitHubIcon from './icons/GitHubIcon';
+import GitHubIcon from '@/icons/GitHubIcon';
 
 interface TeamMember {
   name: string;
