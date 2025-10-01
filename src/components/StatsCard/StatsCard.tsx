5d4f7f4754153fe0247040ef96a9e7a9fb2d7d27
David Eastmond <davebuck1981@gmail.com>
2025-10-01 14:36:28 -0400

    Refactor StatsCard component to remove auxiliaryStat and update styling for subtitle

--- src/components/StatsCard/StatsCard.tsx
+++ src/components/StatsCard/StatsCard.tsx
@@ -1,31 +1,27 @@
-const statsCardStyle = 'card bg-base-100 w-full px-3 py-2 h-28 md:h-full';
+//const statsCardStyle = 'card bg-base-100 w-full px-3 py-2 h-28 md:h-full';
 
 type StatsCardProps = {
   title: string;
   value: string;
-  auxiliaryStat?: {
-    title: string;
-    colorAttribute?: ColorAttribute;
-  };
+  subtitle?: string;
+  color: ColorAttribute;
 };
 
-type ColorAttribute = 'positive' | 'negative' | 'neutral' | 'other';
+type ColorAttribute = 'positive' | 'negative' | string;
+
 export default function StatsCard({
   title,
   value,
-  auxiliaryStat,
+  subtitle,
+  color,
 }: StatsCardProps) {
   return (
-    <div className={statsCardStyle}>
-      <div className="text-lg font-medium">{title}</div>
-      <div className="flex gap-1 items-baseline">
-        <div className="text-4xl font-bold lg:text-6xl">{value}</div>
-        {auxiliaryStat && (
-          <div
-            className={`text-sm ${getColorAttr(auxiliaryStat.colorAttribute)} lg:text-lg`}
-          >
-            {auxiliaryStat.title}
-          </div>
+    <div className="stats shadow h-full w-full">
+      <div className="stat">
+        <div className="stat-title">{title}</div>
+        <div className="stat-value">{value}</div>
+        {subtitle && (
+          <div className={`stat-desc ${getColorAttr(color)}`}>{subtitle}</div>
         )}
       </div>
     </div>
@@ -33,9 +29,8 @@ export default function StatsCard({
 }
 
 function getColorAttr(colorAttribute?: ColorAttribute) {
-  if (colorAttribute === 'positive') return 'text-green-500';
-  if (colorAttribute === 'negative') return 'text-red-500';
-  if (colorAttribute === 'neutral') return 'text-yellow-500';
-  if (colorAttribute === 'other') return 'text-white';
-  return '';
+  if (colorAttribute === 'positive') return 'text-success';
+  if (colorAttribute === 'negative') return 'text-error';
+  if (colorAttribute === 'neutral') return 'text-warning';
+  return 'text-neutral-content';
 }
