d7f69f94b64f62aebe00f83469af57201113ca0e
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-10-02 18:22:52 -0400

    add pull requests data to component

--- src/components/PullRequestsList/PullRequestsList.tsx
+++ src/components/PullRequestsList/PullRequestsList.tsx
@@ -3,53 +3,55 @@
 import { useEffect, useState } from 'react';
 import { useAuth } from '@/hooks/useAuth';
 import { api } from '@/lib/github/client';
-import type { Repo } from '@/db/schema';
+import { PrCard } from '@/components/PrCard/PrCard';
 
 export default function PullRequestsList() {
   const { user, isAuthenticated } = useAuth();
   const [pullRequests, setPullRequests] = useState<GitHubPullRequest[]>([]);
 
-  // getPullRequestsForRepo
-
   useEffect(() => {
     if (!isAuthenticated || !user) return;
 
-    const usernamePromise = api
-      .getAuthenticatedGitHubUser()
-      .then(authenticatedUser => authenticatedUser.login);
+    const promises = fetch(`/api/users/${user.id}/repos`)
+      .then(response => {
+        return response.json();
+      })
+      .then(repos => {
+        if (repos.length === 0) return;
 
-    const pullRequestsPromise = usernamePromise.then(username =>
-      fetch(`/api/users/${user.id}/repos`)
-        .then(response => {
-          return response.json();
-        })
-        .then(repos => {
-          // setRepos(repos);
-          if (repos.length === 0) return;
+        // Fetch all pull requests in parallel
+        const promises = repos.map((repo: GitHubRepo) =>
+          api.getPullRequestsForRepo({
+            owner: repo.owner.login,
+            repo: repo.name,
+          })
+        );
 
-          // Fetch all pull requests in parallel
-          const promises = repos.map((repo: Repo) =>
-            api.getPullRequestsForRepo({
-              owner: username,
-              repo: repo.id,
-            })
-          );
+        return Promise.all(promises);
+      });
 
-          return Promise.all(promises);
-        })
-    );
-
-    pullRequestsPromise.then(pullRequests => {
-      console.log({ pullRequests });
-    });
+    promises
+      .then(pullRequests => {
+        if (!pullRequests) {
+          console.warn('Missing response data');
+          return;
+        }
+        const combined = pullRequests.map(i => i.data).flat();
+        setPullRequests(combined);
+      })
+      .catch(error => {
+        console.log({ error });
+      });
   }, [user, isAuthenticated]);
 
   return (
     <div
       data-label="PullRequestsList"
-      className="flex-grow w-full px-3 py-2 bg-[blue] overflow-y-auto"
+      className="flex flex-col flex-1 gap-2 w-full min-h-0 overflow-y-auto"
     >
-      List of Pull Requests {pullRequests.length}
+      {pullRequests.map(props => (
+        <PrCard key={props.id} {...props} />
+      ))}
     </div>
   );
 }
