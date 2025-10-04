0d7560bdf85fc96a97fa836ff360050323590cf9
David Eastmond <davebuck1981@gmail.com>
2025-10-04 14:37:35 -0400

    Refactor calculateAverageReviewsPerPr function for improved readability and performance

--- src/components/AveragePrReviewsWidget/calculateAverageReviewsPerPr.ts
+++ src/components/AveragePrReviewsWidget/calculateAverageReviewsPerPr.ts
@@ -2,50 +2,46 @@ import { api } from '@/lib/github/client';
 export async function calculateAverageReviewsPerPr(
   userRepos: GitHubRepo[]
 ): Promise<number> {
-  ///const userRepos = await fetchUserRepos();
-  const pullRequests = userRepos.map(repo =>
-    api.getPullRequestsForRepo({
-      owner: repo.owner.login,
-      repo: repo.name,
-      state: 'open',
-    })
-  );
-  const allPullRequestsAcrossRepos = (await Promise.all(
-    pullRequests
+  const pullRequests = (await Promise.all(
+    userRepos.map(repo =>
+      api.getPullRequestsForRepo({
+        owner: repo.owner.login,
+        repo: repo.name,
+        state: 'open',
+      })
+    )
   )) as Array<{ data: GitHubPullRequest[] | null; success: boolean }>;
 
-  const flattenedPullRequests = allPullRequestsAcrossRepos.flatMap(
-    prs => prs.data || []
+  const flattenedPullRequests = pullRequests.flatMap(prs => prs.data || []);
+
+  const reviewPromises = await Promise.allSettled(
+    flattenedPullRequests.map(pr =>
+      api.getReviewsForPullRequest({
+        owner: pr.base.repo.owner.login,
+        repo: pr.base.repo.name,
+        pull_number: pr.number,
+      })
+    )
   );
 
-  const reviews = flattenedPullRequests.map(pr =>
-    api.getReviewsForPullRequest({
-      owner: pr.base.repo.owner.login,
-      repo: pr.base.repo.name,
-      pull_number: pr.number,
-    })
-  );
+  return computeAveragePrReviews(reviewPromises, flattenedPullRequests);
+}
 
-  const allReviews = await Promise.allSettled(reviews);
-  const successFullReviews = allReviews.filter(
-    r => r.status === 'fulfilled'
-  ) as PromiseFulfilledResult<{
-    data: GitHubPullRequestReview[] | null;
-    success: boolean;
-  }>[];
-
-  const totalReviews = successFullReviews.reduce((acc, curr) => {
-    if (curr.value.success && curr.value.data) {
-      return acc + curr.value.data.length;
-    }
-    return acc;
-  }, 0);
+export function computeAveragePrReviews(
+  reviewPromises: PromiseSettledResult<Result<GitHubPullRequestReview[]>>[],
+  pullRequests: GitHubPullRequest[]
+) {
+  const totalReviews = reviewPromises
+    .filter(r => r.status === 'fulfilled')
+    .reduce((acc, curr) => {
+      if (curr.value.success && curr.value.data) {
+        return acc + curr.value.data.length;
+      }
+      return acc;
+    }, 0);
 
   const averageReviews =
-    flattenedPullRequests.length > 0
-      ? totalReviews / flattenedPullRequests.length
-      : 0;
+    pullRequests.length > 0 ? totalReviews / pullRequests.length : 0;
 
-  // setCalculatedAverage(averageReviews);
   return averageReviews;
 }
