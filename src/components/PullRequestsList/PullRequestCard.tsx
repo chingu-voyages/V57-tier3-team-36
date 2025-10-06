dbea7e944b65a92dcd5ee546c7bf48cd063183e7
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-10-06 18:55:57 -0400

    show draft PRs in list

--- src/components/PullRequestsList/PullRequestCard.tsx
+++ src/components/PullRequestsList/PullRequestCard.tsx
@@ -10,7 +10,8 @@ export function PullRequestCard({
   user,
   repo,
 }: GitHubPullRequest & { repo: string }) {
-  if (draft) return null;
+  const badgeStyle = 'badge badge-xs' as const;
+
   return (
     <li className="min-h-20">
       <Link
@@ -19,7 +20,12 @@ export function PullRequestCard({
         rel="noopener noreferrer"
         className="flex flex-col items-start p-2"
       >
-        <h1 className="text-lg">{title}</h1>
+        <h1 className="text-lg flex flex-wrap items-center gap-x-2">
+          {title}
+          {draft ? (
+            <span className={`${badgeStyle} badge-info`}>draft</span>
+          ) : null}
+        </h1>
         <span className="text-sm font-thin mb-1 opacity-[0.7]">
           {repo} #{number} opened {relativeTime(created_at)} by {user.login}
         </span>
