7e3290dcb598204ad808ae7e4a0f91b859feafff
Kelly Ripple <11916341+kripple@users.noreply.github.com>
2025-10-06 13:59:04 -0400

    [MGF-61]: List of Pull Requests (#56)
    * add pull requests data to component
    
    * add placeholder buttons and loading state
    
    * extract PR fetching logic as reusable hook

---
+++ src/hooks/usePullRequests.ts
@@ -0,0 +1,55 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useAuth } from '@/hooks/useAuth';
+import { api } from '@/lib/github/client';
+
+export function usePullRequests() {
+  const { user, isAuthenticated } = useAuth();
+  const [pullRequests, setPullRequests] =
+    useState<(GitHubPullRequest & { repo: string; hasNextPage?: boolean })[]>();
+
+  useEffect(() => {
+    if (!isAuthenticated || !user) return;
+
+    const fetchPullRequests = async () => {
+      try {
+        const repos = await fetch(`/api/users/${user.id}/repos`).then(
+          response => {
+            return response.json();
+          }
+        );
+
+        if (repos.length === 0) return;
+
+        // Fetch all pull requests in parallel
+        const promises = repos.map(async (repo: GitHubRepo) => {
+          const response = await api.getPullRequestsForRepo({
+            owner: repo.owner.login,
+            repo: repo.name,
+          });
+
+          if (!response.success) return [];
+
+          return response.data.map(pullRequest => ({
+            ...pullRequest,
+            repo: repo.name,
+          }));
+        });
+
+        const pullRequests = (await Promise.all(promises)).flat();
+        setPullRequests(pullRequests);
+      } catch (error) {
+        console.error(
+          'Failed to fetch pull requests:',
+          error instanceof Error ? error.message : error
+        );
+      }
+    };
+
+    setPullRequests(undefined);
+    fetchPullRequests();
+  }, [user, isAuthenticated]);
+
+  return pullRequests;
+}
