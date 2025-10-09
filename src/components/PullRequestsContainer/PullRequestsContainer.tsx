e4a1540c0181137a1c9b70c05469569eaded2167
Hyun Woo Kim <hynwkm@gmail.com>
2025-10-08 20:25:36 -0400

    fix: adjust for layout test

--- src/components/PullRequestsContainer/PullRequestsContainer.tsx
+++ src/components/PullRequestsContainer/PullRequestsContainer.tsx
@@ -227,9 +227,11 @@ export default function PullRequestsContainer() {
         />
       </div>
       {isLoading ? (
-        <div className="skeleton flex-1 min-h-0 rounded-box outline outline-offset-1" />
+        <div className="menu flex flex-col flex-1 w-full min-h-0 rounded-box outline outline-offset-1 p-0 divide-y overflow-hidden">
+          <div className="skeleton h-full w-full rounded-box" />
+        </div>
       ) : !sortedPRs || sortedPRs.length === 0 ? (
-        <div className="flex-1 min-h-0 rounded-box outline outline-offset-1">
+        <div className="menu flex flex-col flex-1 w-full min-h-0 rounded-box outline outline-offset-1 p-0 divide-y overflow-y-auto">
           <p className="m-4 text-center">No pull requests found</p>
         </div>
       ) : (
