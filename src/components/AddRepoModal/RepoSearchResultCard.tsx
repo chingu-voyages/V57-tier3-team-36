b4ae5c7a126412c47f8db613969045c95a632ba5
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-10-11 16:12:58 -0400

    id should be defined

--- src/components/AddRepoModal/RepoSearchResultCard.tsx
+++ src/components/AddRepoModal/RepoSearchResultCard.tsx
@@ -34,7 +34,7 @@ export function RepoSearchResultCard({
             <div className="group">
               <button
                 className="btn btn-success"
-                onClick={() => removeRepo?.(id!.toString())}
+                onClick={() => removeRepo?.(id.toString())}
               >
                 <span className="block group-hover:hidden">Added</span>
                 <span className="hidden group-hover:block">Remove</span>
